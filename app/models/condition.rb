9b640b69035f5b9fba17ec85142640931d44e401
mmbensalah <mmbensalah@gmail.com>
2018-09-08 16:37:02 -0600

    TESTS and CREATES functionality for visitor viewing condition show page, TESTS and ADDS validations for condition class

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,2 +1,10 @@
 class Condition < ApplicationRecord
+  validates_presence_of :date,
+                        :max_temperature,
+                        :min_temperature,
+                        :mean_temperature,
+                        :mean_humidity,
+                        :mean_visibility,
+                        :mean_wind_speed,
+                        :precipitation
 end
