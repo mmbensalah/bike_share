1382fadfb4a63af2d6ee80d4ebe2afd493d10769
dmeskis <dmeskis@gmail.com>
2018-09-08 11:25:34 -0600

    Created migration for station slug

--- spec/features/visitor_can_see_a_single_station_spec.rb
+++ spec/features/visitor_can_see_a_single_station_spec.rb
@@ -4,7 +4,8 @@ describe 'visitor can see a station show page' do
   it 'displays a stations attributes' do
     station = create(:station)
 
-    visit station_path(station)
+    visit station_path(station.slug)
+    expect(current_path).to eq("/station/#{station.slug}")
 
     expect(page).to have_content(station.name)
     expect(page).to have_content(station.dock_count)
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Station, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it {should validate_uniqueness_of(:name)}
+    it { should validate_presence_of(:dock_count)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:installation_date)}
+  end
 end
