b7b39fcba84c7df0f134aa968e0c65b6f16482ac
dmeskis <dmeskis@gmail.com>
2018-09-09 10:56:51 -0600

    Fixed factorybot to be dynamic

--- spec/factories/conditions.rb
+++ spec/factories/conditions.rb
@@ -1,13 +1,13 @@
 require 'faker'
 FactoryBot.define do
   factory :condition do
-    date Faker::Date.between(2.days.ago, Date.today)
-    max_temperature Faker::Number.between(0, 115)
-    min_temperature Faker::Number.between(0, 115)
-    mean_temperature Faker::Number.between(0, 115)
-    mean_humidity Faker::Number.between(0, 100)
-    mean_visibility Faker::Number.between(0, 50)
-    mean_wind_speed Faker::Number.between(0, 50)
-    precipitation Faker::Number.between(0, 50)
+    date { Faker::Date.between(2.days.ago, Date.today) }
+    max_temperature { Faker::Number.between(0, 115) }
+    min_temperature { Faker::Number.between(0, 115) }
+    mean_temperature { Faker::Number.between(0, 115) }
+    mean_humidity { Faker::Number.between(0, 100) }
+    mean_visibility { Faker::Number.between(0, 50) }
+    mean_wind_speed { Faker::Number.between(0, 50) }
+    precipitation { Faker::Number.between(0, 50) }
   end
 end
--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
   factory :trip do
     duration { Faker::Number.between(120, 20000) }
     start_date { Faker::Time.between(DateTime.now - 1, DateTime.now) }
-    end_date DateTime.now
+    end_date { DateTime.now }
     bike_id { Faker::Number.between(1, 100) }
-    subscription_type "Placeholder"
+    subscription_type { "Placeholder" }
     zip_code { Faker::Number.number(5) }
     association :start_station, factory: :station
     association :end_station, factory: :station
