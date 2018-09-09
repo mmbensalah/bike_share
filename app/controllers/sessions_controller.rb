4ea6d241bec9af6b91046716b6b7a0374cfd34e7
mmbensalah <mmbensalah@gmail.com>
2018-09-09 13:47:35 -0600

    TESTS user can logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = "You have successfully logged out."
+    redirect_to root_path
+  end
 end
