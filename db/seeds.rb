dcc17718dd7aee446d8c299aa3acf9bc43e6a41b
dmeskis <dmeskis@gmail.com>
2018-09-10 15:24:50 -0600

    Added csvs, seperated seed files into development and production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'csv'
-require 'time'
 
-CSV.foreach("./db/csv/station.csv", headers: true, header_converters: :symbol) do |row|
-  Station.create(             name:                    row[:name],
-                              dock_count:              row[:dock_count],
-                              city:                    row[:city],
-                              installation_date:       Date.strptime(row[:installation_date],"%m/%d/%Y")
-                            )
-end
-CSV.foreach("./db/csv/weather.csv", headers: true, header_converters: :symbol) do |row|
-  Condition.find_or_create_by!(
-                    date:                         Date.strptime(row[:date],"%m/%d/%Y"),
-                    max_temperature:              row[:max_temperature].to_i,
-                    min_temperature:              row[:min_temperature].to_i,
-                    mean_temperature:             row[:mean_temperature].to_i,
-                    mean_humidity:                row[:mean_humidity].to_i,
-                    mean_visibility:              row[:mean_visibility_miles].to_i,
-                    mean_wind_speed:              row[:mean_wind_speed_mph].to_i,
-                    precipitation:                 row[:precipitation_inches].to_f
-                    )
-end
+# google this thing below
+# how to use seed rb to selectively populate development and or production databas
+# production:
+
+# development:
+# require 'csv'
+# require 'time'
+
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
+
+#
+# CSV.foreach("./db/csv/station.csv", headers: true, header_converters: :symbol) do |row|
+#   Station.create(             name:                    row[:name],
+#                               dock_count:              row[:dock_count],
+#                               city:                    row[:city],
+#                               installation_date:       Date.strptime(row[:installation_date],"%m/%d/%Y")
+#                             )
+# end
+# CSV.foreach("./db/csv/weather.csv", headers: true, header_converters: :symbol) do |row|
+#   Condition.find_or_create_by!(
+#                     date:                         Date.strptime(row[:date],"%m/%d/%Y"),
+#                     max_temperature:              row[:max_temperature].to_i,
+#                     min_temperature:              row[:min_temperature].to_i,
+#                     mean_temperature:             row[:mean_temperature].to_i,
+#                     mean_humidity:                row[:mean_humidity].to_i,
+#                     mean_visibility:              row[:mean_visibility_miles].to_i,
+#                     mean_wind_speed:              row[:mean_wind_speed_mph].to_i,
+#                     precipitation:                 row[:precipitation_inches].to_f
+#                     )
+# end
