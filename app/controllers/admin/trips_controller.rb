c1c9c8aa9946dfea2fc14f1e0aec9f3bf9344696
dmeskis <dmeskis@gmail.com>
2018-09-10 17:15:55 -0600

    Admin can create a trip

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -10,10 +10,19 @@ class Admin::TripsController < Admin::BaseController
 
   def new
     @trip = Trip.new
+    @stations = Station.all
   end
 
   def create
-
+    require 'pry'; binding.pry
+    @trip = Trip.create!(trip_params)
+    if @trip.save
+      flash[:success] = "You successfully created a trip!"
+      redirect_to admin_trip_path(@trip)
+    else
+      flash[:failure] = "Invalid information, please fill out the form again."
+      render :new
+    end
   end
 
   def destroy
@@ -25,4 +34,16 @@ class Admin::TripsController < Admin::BaseController
       redirect_to admin_trip_path(@trip)
     end
   end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:duration,
+                                   :start_date,
+                                   :end_date,
+                                   :bike_id,
+                                   :subscription_type,
+                                   :zip_code,
+                                   :start_station_id,
+                                   :end_station_id)
+    end
 end
--- spec/features/admin/admin_can_create_a_new_trip_spec.rb
+++ spec/features/admin/admin_can_create_a_new_trip_spec.rb
@@ -6,18 +6,18 @@ describe 'admin can create new trip' do
         appears showing I succesfully created a trip' do
         admin = create(:user, role:1)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
+        station_1 = create(:station)
+        station_2 = create(:station)
 
         visit new_admin_trip_path
-
         fill_in :trip_duration, with: 500
-        fill_in :trip_start_date, with: "09-15-1992"
-        fill_in :trip_end_date, with: "10-31-2007"
+        fill_in :trip_start_date, with: "1992-04-01"
+        fill_in :trip_end_date, with: "2007-09-15"
         fill_in :trip_bike_id, with: 7
         fill_in :trip_subscription_type, with: "Subscriber"
         fill_in :trip_zip_code, with: 80220
-        fill_in :trip_start_station_id, with: 1
-        fill_in :trip_end_station_id, with: 2
+        select(value = station_1.name, from: 'trip[start_station_id]')
+        select(value = station_2.name, from: 'trip[end_station_id]')
 
         click_on "Create Trip"
         expect(Trip.all.count).to eq(1)
