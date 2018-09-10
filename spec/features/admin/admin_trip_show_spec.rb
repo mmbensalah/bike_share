012d7a2f981d9d0ce43542f558d8bbcb37ae0f32
dmeskis <dmeskis@gmail.com>
2018-09-10 16:00:36 -0600

    Wrote tests for admin trip show page

---
+++ spec/features/admin/admin_trip_show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'admin visits a trip show page' do
+  describe 'sees everything a user can see' do
+    it 'can also see a button to edit a trip' do
+      trip = create(:trip)
+
+      visit admin_trip_path(trip)
+
+      click_on "Edit Trip"
+
+      expect(current_path).to eq admin_edit_trip_path(trip)
+    end
+    it 'can also see a button to delete a trip' do
+      trip = create(:trip)
+
+      visit admin_trip_path(trip)
+
+      click_on "Delete Trip"
+
+      expect(Trip.all.count).to eq(0)
+    end
+  end
+end
