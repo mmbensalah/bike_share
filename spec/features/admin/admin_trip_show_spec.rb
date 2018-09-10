5892a9fac120865f2d4eaf00fe5907d93b287655
dmeskis <dmeskis@gmail.com>
2018-09-10 16:04:09 -0600

    Added admin privelege to tests

--- spec/features/admin/admin_trip_show_spec.rb
+++ spec/features/admin/admin_trip_show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'admin visits a trip show page' do
   describe 'sees everything a user can see' do
     it 'can also see a button to edit a trip' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       trip = create(:trip)
 
       visit admin_trip_path(trip)
@@ -12,6 +13,7 @@ describe 'admin visits a trip show page' do
       expect(current_path).to eq admin_edit_trip_path(trip)
     end
     it 'can also see a button to delete a trip' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       trip = create(:trip)
 
       visit admin_trip_path(trip)
