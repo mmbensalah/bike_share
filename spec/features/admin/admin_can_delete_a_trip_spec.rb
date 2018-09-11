fcd4471e43319c4b6763e1df1ac3c39ed832443c
dmeskis <dmeskis@gmail.com>
2018-09-10 19:36:01 -0600

    Adds admin can delete trip test

---
+++ spec/features/admin/admin_can_delete_a_trip_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'admin can delete a trip' do
+  describe 'on admin trip index page' do
+    it 'clicks delete, and it returns admin to index with trip destroyed' do
+        admin = create(:user, role:1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        trip = create(:trip)
+
+        visit trips_path
+        click_on "Delete"
+
+        expect(current_path).to eq admin_trips_path
+        expect(page).to have_content("Trip deleted!")
+      end
+    end
+  end
