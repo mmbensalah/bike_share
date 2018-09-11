550832caef4461f5c4a4b3d5f483ccda7c9153d7
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-11 14:07:53 -0600

    Adds admin factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,4 +9,13 @@ FactoryBot.define do
     password { Faker::Internet.password }
     role { 0 }
   end
+
+  factory :admin, :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    username { Faker::Internet.username }
+    password { Faker::Internet.password }
+    role { 1 }
+  end
 end
