159a8a2770d199609905a532f01af1e9fd4af449
dmeskis <dmeskis@gmail.com>
2018-09-11 16:03:18 -0600

    Changed trip_path to condition_path (damn copy paste)

--- app/controllers/admin/conditions_controller.rb
+++ app/controllers/admin/conditions_controller.rb
@@ -8,7 +8,7 @@ class Admin::ConditionsController < Admin::BaseController
     @condition = Condition.find(params[:id])
     if @condition.update(condition_params)
       flash[:success] = "You successfully edited a condition!"
-      redirect_to trip_path(@condition)
+      redirect_to condition_path(@condition)
     else
       flash[:failure] = "Invalid information, please fill out the form again."
       render :edit
@@ -25,7 +25,7 @@ class Admin::ConditionsController < Admin::BaseController
       redirect_to condition_path(@condition)
     end
   end
-  
+
 
   def new
     @condition = Condition.new
