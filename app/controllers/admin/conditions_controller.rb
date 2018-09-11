2472c384d38a788db79e9fb97b1d4e6cf4d7de84
dmeskis <dmeskis@gmail.com>
2018-09-11 15:47:40 -0600

    Fixed show by removing unnecessary variable

--- app/controllers/admin/conditions_controller.rb
+++ app/controllers/admin/conditions_controller.rb
@@ -19,4 +19,28 @@ class Admin::ConditionsController < Admin::BaseController
     @condition = Condition.new
   end
 
+  def create
+    @condition = Condition.create(condition_params)
+    if @condition.save
+      flash[:success] = "You successfully created a condition!"
+      redirect_to condition_path(@condition)
+    else
+      flash[:failure] = "Invalid information, please fill out the form again."
+      render :new
+    end
+  end
+
+  private
+    def condition_params
+      params.require(:condition).permit(:date,
+                                        :max_temperature,
+                                        :min_temperature,
+                                        :mean_temperature,
+                                        :mean_humidity,
+                                        :mean_visibility,
+                                        :mean_wind_speed,
+                                        :precipitation
+                                        )
+    end
+
 end
