ed77a0320bc0b60ef48f76bf5bd01b65ecf2c85b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-10 18:03:21 -0600

    Adds cart to ApplicationController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
+    @cart.add_item(item.id)
+    session[:cart] = @cart.contents
     flash[:success] = "You have successfully added #{item.title} to your cart."
     redirect_to bike_shop_path
   end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,7 +6,17 @@ describe Cart do
 
   describe "#total_count" do
     it 'should return total count of items in cart' do
-      expect(subject.total_count).to eq(5)
+      expect(subject.total_count).to eq(3)
+    end
+  end
+
+  describe "#add_item" do
+    it 'should add an item to the cart' do
+
+      subject.add_item(1)
+      subject.add_item(2)
+
+      expect(subject.contents).to eq({"1" => 2, "2" => 3})
     end
   end
 end
