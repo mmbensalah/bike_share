6988812d12551a98bd8b0a16bec9aff44ff7fd2b
mmbensalah <mmbensalah@gmail.com>
2018-09-11 19:40:55 -0600

    Basic files added to conditions db

---
+++ spec/features/user/user_can_see_conditions_dashboard_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Conditions Dashboard' do
+  describe 'user can visit conditions dashboard path' do
+    it 'renders condition dashboard for user' do
+      user = create(:user)
+
+      visit conditions_dashboard_path
+
+      expect(page).to have_content('Conditions Dashboard')
+    end
+
+
+    end
+
+  end
+end
