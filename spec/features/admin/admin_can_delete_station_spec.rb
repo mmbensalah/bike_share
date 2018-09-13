6294ea32988f8ed34d56a7c78231d74e975cfc75
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-12 21:50:06 -0600

    Adds test to delete station from show

--- spec/features/admin/admin_can_delete_station_spec.rb
+++ spec/features/admin/admin_can_delete_station_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 describe "As an admin" do
-  describe "when I visit the stations index" do
-    it 'should be able to delete a station' do
+  describe "I want to delete a station" do
+    it 'should be able to delete a station from the index' do
       admin = create(:user, role: 1)
       station_1 = create(:station)
       station_2 = create(:station)
@@ -21,5 +21,24 @@ describe "As an admin" do
       end
       expect(page).to have_content(station_2.name)
     end
+
+    it 'should be able to delete a station from the show' do
+      admin = create(:user, role: 1)
+      station = create(:station)
+      station_2 = create(:station)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit station_path(station)
+
+      click_on("Delete Station")
+
+      expect(page).to eq(stations_path)
+      expect(page).to have_content("You have succesfully deleted Station #{station_1.name}.")
+      within(".stations") do
+        expect(page).to_not have_content(station.name)
+        expect(page).to have_content(station_2.name)
+      end
+    end 
   end
 end
