cc210b6844cf2b0b5314d4759ecd21d82ae22b39
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-12 21:29:51 -0600

    tests for failure of creating new station

--- spec/features/admin/stations/admin_can_create_new_station_spec.rb
+++ spec/features/admin/stations/admin_can_create_new_station_spec.rb
@@ -29,5 +29,26 @@ describe "As an admin" do
       expect(page).to have_content(city)
       expect(page).to have_content("Installation Date: #{date}")
     end
+
+    it 'should not save if name is duplicated' do
+      admin = create(:user, role: 1)
+      station = create(:station)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit new_admin_station_path
+
+      dock_count = 30
+      city = "Daly City"
+      date = "2018-09-15"
+
+      fill_in :station_name, with: station.name
+      fill_in :station_dock_count, with: dock_count
+      fill_in :station_city, with: city
+      fill_in :station_installation_date, with: date
+      click_on("Create Station")
+
+      expect(current_path).to eq(admin_stations_path)
+      expect(page).to have_content("Your station was not saved. Please try again.")
+    end
   end
 end
