7e798c7188c25cfdf98bf78942829890f49380d7
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-13 10:17:20 -0600

    adds setup to station model test

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Station, type: :model do
+  before(:each) do
+    @station_1 = Station.create!(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "2013-08-06")
+    @station_2 = Station.create!(name: "Castro Station", dock_count: 25, city: "San Francisco", installation_date: "2013-08-06")
+    @station_3 = Station.create!(name: "SFSU", dock_count: 25, city: "Daly City", installation_date: "2013-08-06")
+    @trip_1 = Trip.create!(duration: 71, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 48, subscription_type: "Subscriber", zip_code: 97214, start_station_id: 1, end_station_id: 1)
+    @trip_2 = Trip.create!(duration: 77, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 26, subscription_type: "Subscriber", zip_code: 94103, start_station_id: 1, end_station_id: 2)
+    @trip_3 = Trip.create!(duration: 1099, start_date: "2013-09-05", end_date: "2013-09-05", bike_id: 48, subscription_type: "Customer", zip_code: 10038, start_station_id: 3, end_station_id: 1)
+    @trip_4 = Trip.create!(duration: 83, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 319, subscription_type: "Subscriber", zip_code: 94103, start_station_id: 1, end_station_id: 3)
+    @trip_5 = Trip.create!(duration: 109, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 679, subscription_type: "Subscriber", zip_code: 95112, start_station_id: 1, end_station_id: 2)
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name)}
     it { should validate_presence_of(:dock_count)}
@@ -15,17 +26,8 @@ RSpec.describe Station, type: :model do
   describe 'instance methods' do
     context '#ride_count' do
       it 'should return the number of rides started/ended at this station' do
-        station_1 = Station.create!(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "2013-08-06")
-        station_2 = Station.create!(name: "Castro Station", dock_count: 25, city: "San Francisco", installation_date: "2013-08-06")
-        station_3 = Station.create!(name: "SFSU", dock_count: 25, city: "Daly City", installation_date: "2013-08-06")
-        trip_1 = Trip.create!(duration: 71, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 48, subscription_type: "Subscriber", zip_code: 97214, start_station_id: 1, end_station_id: 1)
-        trip_2 = Trip.create!(duration: 77, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 26, subscription_type: "Subscriber", zip_code: 94103, start_station_id: 1, end_station_id: 2)
-        trip_3 = Trip.create!(duration: 1099, start_date: "2013-09-05", end_date: "2013-09-05", bike_id: 48, subscription_type: "Customer", zip_code: 10038, start_station_id: 3, end_station_id: 1)
-        trip_4 = Trip.create!(duration: 83, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 319, subscription_type: "Subscriber", zip_code: 94103, start_station_id: 1, end_station_id: 3)
-        trip_5 = Trip.create!(duration: 109, start_date: "2013-08-29", end_date: "2013-08-29", bike_id: 679, subscription_type: "Subscriber", zip_code: 95112, start_station_id: 1, end_station_id: 2)
-
-        expect(station_1.ride_count("started")).to eq(4)
-        expect(station_1.ride_count("ended")).to eq(2)
+        expect(@station_1.ride_count("started")).to eq(4)
+        expect(@station_1.ride_count("ended")).to eq(2)
       end
     end
   end
