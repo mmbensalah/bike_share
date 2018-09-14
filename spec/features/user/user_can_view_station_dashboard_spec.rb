48db47276c9d0f7cdb73ca5edc99bf5926b965cd
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-14 17:51:18 -0600

    Adds test to view stations dashboard

---
+++ spec/features/user/user_can_view_station_dashboard_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe "As an authorized user" do
+  describe "when I visit the welcome page" do
+    it 'has a link for the station dashboard' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit welcome_path
+      click_on("Stations Dashboard")
+
+      expect(current_path).to eq(stations_dashboard_path)
+      expect(page).to have_content("Stations Dashboard")
+    end
+  end
+end
