ee99bc3a0fdd4fa60cc277c5ccced76198ab4dae
mmbensalah <mmbensalah@gmail.com>
2018-09-13 18:06:30 -0600

    CREATES functionalit for admin creating new item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  validates_presence_of :title
-  validates_presence_of :price
+  validates :title, uniqueness: true, presence: true
   validates_presence_of :image
+  validates_presence_of :price
   validates_presence_of :description
   validates_presence_of :status
   enum status: %w(active retired)
