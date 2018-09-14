e0c1217ee74342934882bea6e1b12d986f910bf4
mmbensalah <mmbensalah@gmail.com>
2018-09-14 09:04:23 -0600

    ADDS ActionView::Helpers::TextHelper to OrdersController to access number_to_currency helper method

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,17 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+  include ActionView::Helpers::NumberHelper
+
 
   def total_price
-    order_items.map do |order_item|
+    number_to_currency(order_items.map do |order_item|
       order_item.price * order_item.quantity
-    end.sum
+    end.sum)
   end
+
+  def helpers
+  ActionController::Base.helpers
+  end
+
 end
