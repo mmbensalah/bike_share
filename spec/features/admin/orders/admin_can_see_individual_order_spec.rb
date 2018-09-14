4d2a7c57cf41c8c8b5842c77eb16b817a2eac77e
dmeskis <dmeskis@gmail.com>
2018-09-14 12:36:48 -0600

    User can now see card on order show with user info

--- spec/features/admin/orders/admin_can_see_individual_order_spec.rb
+++ spec/features/admin/orders/admin_can_see_individual_order_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe 'admin can visit individual order show page' do
   it 'shows all the details for this item' do
     order = create(:order)
+    admin = order.user
+    order.user.role = 1
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     visit order_path(order)
     save_and_open_page
     expect(page).to have_content("#{order.user.first_name}")
