53bf0149b805093e411a9a25a7c99a90db73c1de
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-15 12:54:01 -0600

    Adds test so user can update account

---
+++ spec/features/user/user_can_edit_account_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+describe "As an authenticated user" do
+  describe "when I visit my account page" do
+    it "should be able to edit account info" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      old_first_name = user.first_name
+      old_last_name = user.last_name
+      old_email = user.email
+      old_address = user.address
+
+      visit dashboard_path
+
+      click_on("Edit my account info")
+
+      new_first_name = "Kat"
+      new_last_name = "Yruegas"
+      new_email = "katy@email.com"
+      new_address = "123 Main St, Denver, CO 80224"
+
+      expect(current_path).to eq(edit_user_path(user))
+      fill_in :user_first_name, with: new_first_name
+      fill_in :user_last_name, with: new_last_name
+      fill_in :user_email, with: new_email
+      fill_in :user_address, with: new_address
+
+      click_on("Update Account")
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(new_first_name)
+      expect(page).to have_content(new_last_name)
+      expect(page).to have_content(new_email)
+      expect(page).to have_content(new_address)
+      expect(page).to_not have_content(old_first_name)
+      expect(page).to_not have_content(old_last_name)
+      expect(page).to_not have_content(old_email)
+      expect(page).to_not have_content(old_address)
+    end
+  end
+end
