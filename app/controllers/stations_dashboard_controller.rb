3492045500ea9b945c08006e4dbd6f9016d6830c
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-14 18:03:37 -0600

    Adds test to restrict access for visitor

--- app/controllers/stations_dashboard_controller.rb
+++ app/controllers/stations_dashboard_controller.rb
@@ -1,4 +1,6 @@
 class StationsDashboardController < ApplicationController
+  before_action :require_user
   def index
-  end 
+
+  end
 end
--- spec/features/user/user_can_view_station_dashboard_spec.rb
+++ spec/features/user/user_can_view_station_dashboard_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
+describe "As a visitor" do
+  it "should not allow access to stations dashboard" do
+    visit root_path
+
+    expect(page).to_not have_content("Stations Dashboard")
+
+    visit stations_dashboard_path
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
+end
+
 describe "As an authorized user" do
   describe "when I visit the welcome page" do
     it 'has a link for the station dashboard' do
