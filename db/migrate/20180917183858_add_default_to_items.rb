71be3836faf2bd0cafb4cebcbc1f6b6885cee2da
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-17 14:09:31 -0600

    adds default image to controller

--- db/migrate/20180917183858_add_default_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToItems < ActiveRecord::Migration[5.1]
-  def change
-    change_column :items, :image, :string, default: 'trail_bike.jpg'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917183858) do
+ActiveRecord::Schema.define(version: 20180914181605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180917183858) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.float "price"
-    t.string "image", default: "trail_bike.jpg"
+    t.string "image"
     t.text "description"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
